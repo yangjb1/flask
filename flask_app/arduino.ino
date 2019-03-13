bd7038a922ba4c3e9a79431b1a71b87e26985108
yangjb1 <yangjb1@mail.uc.edu>
2019-03-13 17:57:47 -0400

    two relays one current sensor and four voltage sensors

--- flask_app/arduino.ino
+++ flask_app/arduino.ino
@@ -1,7 +1,13 @@
-#define RELAY1  6
-#define RELAY2  7
-#define RELAY3  8
-#define RELAY4  9
+#define RELAY1  6 // modem
+#define RELAY2  7 // analog streamer
+
+/*
+A0-current
+A1-v1
+A2-v2
+A3-v3
+A4-v4
+*/
 
 float vout = 0.0;
 float vin = 0.0;
@@ -11,8 +17,6 @@ int value = 0;
 
 bool RELAY1_status = 1;
 bool RELAY2_status = 1;
-bool RELAY3_status = 1;
-bool RELAY4_status = 1;
 
 void setup() {
     // put your setup code here, to run once:
@@ -22,8 +26,8 @@ void setup() {
     pinMode(RELAY3, OUTPUT);
     pinMode(RELAY4, OUTPUT);
 
-    //digitalWrite( RELAY1, LOW); // ON
-    //digitalWrite( RELAY2, LOW); // ON
+    digitalWrite( RELAY1, HIGH); // OFF
+    digitalWrite( RELAY2, HIGH); // OFF
     //digitalWrite( RELAY3, LOW); // ON
     //digitalWrite( RELAY4, LOW); // ON
 
@@ -37,9 +41,15 @@ void loop() {
     // wait until the serial connection is open
     while (Serial.available() == 0){
     
-    int value = analogRead(A1);
+    int value1 = analogRead(A1);
+    int value2 = analogRead(A2);
+    int value3 = analogRead(A3);
+    int value4 = analogRead(A4);
     //Serial.println(value);
-    float v = value*((5.0*(R1+R2))/(1023.0*R2));
+    float v1 = value1*((5.0*(R1+R2))/(1023.0*R2));
+    float v2 = value2*((5.0*(R1+R2))/(1023.0*R2));
+    float v3 = value3*((5.0*(R1+R2))/(1023.0*R2));
+    float v4 = value4*((5.0*(R1+R2))/(1023.0*R2));
 
    
     float average = 0;
@@ -48,19 +58,25 @@ void loop() {
         //   average = average + (.049 * analogRead(A0) -25);// for 20A mode
         // average = average + (.742 * analogRead(A0) -37.8);// for 30A mode
     }
-    Serial.print(v,2);
-    Serial.print(",");
     Serial.print(average,2);
+    Serial.print(",");
+    Serial.print(v1,2);
+    Serial.print(",");
+    Serial.print(v2,2);
+    Serial.print(",");
+    Serial.print(v3,2);
+    Serial.print(",");
+    Serial.print(v4,2);
+    Serial.print(",");
     Serial.print(RELAY1_status);
-    Serial.print(RELAY2_status);
-    Serial.print(RELAY3_status);
-    Serial.print(RELAY4_status);
+    Serial.print(",");
+    Serial.println(RELAY2_status);
     }
 
     //read from the serial connection; the - '0' 
     //is to cast the values as the int and not the ASCII code
     int COM_value = Serial.read() - '0';
-    Serial.println(COM_value); //print to the console for testing
+    //Serial.println(COM_value); //print to the console for testing
     if (COM_value == RELAY1) {
         if (RELAY1_status) {
             digitalWrite( RELAY1, HIGH); // OFF
@@ -76,24 +92,6 @@ void loop() {
         } else {
             digitalWrite( RELAY2, LOW); // ON
             RELAY2_status = 1;
-        }
-    } else if (COM_value == RELAY3) {
-        if (RELAY3_status) {
-            digitalWrite( RELAY3, HIGH); // OFF
-            RELAY3_status = 0;
-        } else {
-            digitalWrite( RELAY3, LOW); // ON
-            RELAY3_status = 1;
-        }
-    } else if (COM_value == RELAY4){
-        if (RELAY4_status) {
-            Serial.println("Received a 9- OFF");
-            digitalWrite( RELAY4, HIGH); // OFF
-            RELAY4_status = 0;
-        } else {
-            Serial.println("Received a 9- ON");
-            digitalWrite( RELAY4, LOW); // ON
-            RELAY4_status = 1;
         }
     }
     /*
